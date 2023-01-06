d657121f838330746ebe3592e2a234cbbe43448a
jyg <yonggyujeong10@gmail.com>
2023-01-07 01:41:52 +0900

    오타 수정

--- src/main/java/NumberBaseballGame/Number.java
+++ src/main/java/NumberBaseballGame/Number.java
@@ -2,27 +2,27 @@ package NumberBaseballGame;
 
 
 public class Number {
-    private final Integer numer;
+    private final Integer number;
 
     public Number() {
         Double randomDouble = Math.floor(Math.random() * 1000);
-        numer = randomDouble.intValue();
+        number = randomDouble.intValue();
     }
 
     public Number(Integer number){
-        this.numer = number;
+        this.number = number;
     }
 
     public Integer get() {
-        return numer;
+        return number;
     }
 
     public String toString() {
-        return numer.toString();
+        return number.toString();
     }
 
     public String[] toStringArray() {
-        String str = numer.toString();
+        String str = number.toString();
         return str.split("");
     }
 }
